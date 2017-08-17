4d33507651cc325c3419764dafa4ad11b078e896
Tom <tom.clement@bizboard.nl>
2017-08-17 02:13:19 +0200

    Fixed param flags being overwritten by defaults

--- scripts/main.go
+++ scripts/main.go
@@ -10,13 +10,13 @@ import (
 )
 
 func main() {
-	screen_width := uint16(*flag.Int("screen_width", 320, "Width of the screen to draw on"))
-	screen_height := uint16(*flag.Int("screen_height", 320, "Height of the screen to draw on"))
-	display := *flag.String("display", "/dev/fb0", "Name of the framebuffer device to write to")
-	profile := *flag.Bool("profile", false, "Set to true to enable CPU profiling > cpu.profile")
+	screen_width := flag.Int("screen_width", 320, "Width of the screen to draw on")
+	screen_height := flag.Int("screen_height", 320, "Height of the screen to draw on")
+	display := flag.String("display", "/dev/fb0", "Name of the framebuffer device to write to")
+	profile := flag.Bool("profile", false, "Set to true to enable CPU profiling > cpu.profile")
 	flag.Parse()
 
-	if profile {
+	if *profile {
 		fmt.Println("Running with CPU profiling enabled")
 		prof_file, err := os.Create("cpu.profile")
 		if err != nil {
@@ -27,13 +27,13 @@ func main() {
 	}
 
 	fmt.Println("Starting server")
-	server := pixelflood_server.NewServer(uint16(screen_width), uint16(screen_height))
+	server := pixelflood_server.NewServer(uint16(*screen_width), uint16(*screen_height))
 	go server.Run()
 	defer server.Stop()
 
 
 	fmt.Println("Starting render thread")
-	renderer := pixelflood_server.NewRenderer(server, display, uint16(screen_width), uint16(screen_height))
+	renderer := pixelflood_server.NewRenderer(server, *display, uint16(*screen_width), uint16(*screen_height))
 	renderer.Initialise()
 	go renderer.Run()
 	defer renderer.Stop()
